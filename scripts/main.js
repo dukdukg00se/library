a2fb8d77b9c97562de249ebf1aac4ea928f90a90
Will H <will.wkhu@gmail.com>
2021-12-19 22:24:00 -0800

    Set css to play with layout, test out some functions

---
+++ scripts/main.js
@@ -0,0 +1,56 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  };
+}
+
+// function addBookToLibrary(bookObj) {
+//   myLibrary.push(bookObj);
+// }
+
+function addBookToLibrary() {
+  let myBook = new Book('IT', 'King', 500, true);
+  myLibrary.push(myBook);
+}
+
+
+function displayBooks(arr) {
+  const library = document.querySelector('#library');
+  for (let i = 0; i < arr.length; i++) {
+    let book = document.createElement('div');
+    book.classList.add('book');
+    let bookTitle = document.createElement('h2');
+    bookTitle.classList.add('bookTitle');
+    // bookTitle.textContent = arr[i].title;
+    // book.appendChild(bookTitle);
+    bookTitle.append(arr[i].title);
+    book.append(bookTitle);
+    let bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('bookAuthor');
+    bookAuthor.append(arr[i].author);
+    book.append(bookAuthor);
+    let bookPages = document.createElement('p');
+    bookPages.classList.add('bookPages');
+    bookPages.append(arr[i].pages);
+    book.append(bookPages);
+
+    library.append(book);
+  }
+}
+
+let black = new Book('black', 'x', 500, true);
+addBookToLibrary(black);
+
+// addBookToLibrary(new Book('Black', 'X', 500, true));
+
+displayBooks(myLibrary);
+
+
+
+
