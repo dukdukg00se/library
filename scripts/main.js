f295130d0a6afcaa4c024ae9a1d8442654c46745
Will H <will.wkhu@gmail.com>
2022-01-04 14:48:12 -0800

    Clean/comment code. Reworked, renamed, relocated code to improve readability

--- scripts/main.js
+++ scripts/main.js
@@ -18,98 +18,82 @@ function store(){
   let author = document.getElementById("author");
   let pages = document.getElementById("pages");
   let read = document.getElementById("read");
-
   let myBook = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(myBook);
 }
 
+// Iterate through myLibrary array and display each book
 function displayBooks() {
+  // Create a subcontainer to append books to
+  // After user deletes a book the subcontainer is removed, book
+  // deleted from array and array displayed again
   let librarySubcontainer = document.createElement('div');
   librarySubcontainer.classList.add('library-subcontainer');
   let library = document.getElementById('library');
 
+  // Iterate myLibrary, creating a card with input info for each book
   for (let i = 0; i < myLibrary.length; i++) {
     let book = document.createElement('div');
-    book.id = 'book' + i;
+    book.id = 'book-' + i;
     book.classList.add('book');
-
-    let closeButton = document.createElement('button');
-    closeButton.classList.add('close');
-    closeButton.type = 'button';
-    closeButton.id = 'close-btn' + i;
-    closeButton.setAttribute('data-value', myLibrary[i].title);
-
-    closeButton.textContent = '\xD7'; // Display special x symbol using hex code
-
+    // Create close button
+    let closeBtn = document.createElement('button');
+    closeBtn.classList.add('close-btn');
+    closeBtn.type = 'button';
+    closeBtn.id = 'close-btn-' + i; // id used in close function
+    closeBtn.setAttribute('data-value', myLibrary[i].title);
+    closeBtn.textContent = '\xD7'; // Display special x symbol using hex code
+    // Create book title header
     let bookTitle = document.createElement('h2');
     bookTitle.classList.add('book-title');
     bookTitle.textContent = myLibrary[i].title;
-
+    // Create p element with author info
     let bookAuthor = document.createElement('p');
     bookAuthor.classList.add('book-author');
     bookAuthor.textContent = myLibrary[i].author;
-
+    // Create p element with book page length
     let bookPages = document.createElement('p');
     bookPages.classList.add('book-pages');
     bookPages.textContent = myLibrary[i].pages;
-
+    // Create p element to display if read 
     let read = document.createElement('p');
     read.classList.add('read');
     read.textContent = myLibrary[i].read;
-
-    book.append(closeButton, bookTitle, bookAuthor, bookPages, read);
+    // Add info to book card
+    book.append(closeBtn, bookTitle, bookAuthor, bookPages, read);
+    // Append book card to subcontainer
     librarySubcontainer.appendChild(book);
   }
-
+  // If library is already displaying books then remove existing cards by removing subcontainer
   if (!!library.firstElementChild) {
     let librarySubcontainer = document.querySelector('.library-subcontainer');
     library.removeChild(librarySubcontainer);
   }
+  // Display books by appending subcontainer to library
   library.appendChild(librarySubcontainer);
-
-  close();
-  // let closeWin = document.querySelectorAll('.close');
-  // closeWin.forEach((btn) => {
-  //   btn.addEventListener('click', (e) => {
-  //     // console.log(e.target.id);
-  //     let childNode = document.getElementById(e.target.id);
-  //     // console.log(childNode.parentNode);
-  //     let parent = childNode.parentNode;
-  //     // console.log(parent.getAttribute('data-nmbr'));
-  //     // console.log(parent.parentNode);
-  //     let grandParent = parent.parentNode;
-  //     grandParent.removeChild(parent);
-
-  //     myLibrary.splice(parent.getAttribute('data-nmbr'), 1);
-    
-  //   });
-  // });
 }
 
-let submit = document.getElementById("submit");
+
+let submit = document.getElementById("submit-btn");
 submit.addEventListener('click', () => {
-  store();
-  displayBooks();
-  
-  let form = document.getElementById('modal-content');
+  // After clicking submit button:
+  store(); // Create new book and add to myLibrary
+  displayBooks(); // Display books in myLibrary
+  close(); // Listen for close button click
+
+  // Reset form and remove 
+  let form = document.getElementById('modal-form');
   form.reset();
   modal.style.display = 'none';
-
-  // let closeWin = document.querySelectorAll('.close');
-  // closeWin.forEach((btn) => {
-  // btn.addEventListener('click', () => {
-  //   console.log(btn.className);
-  //   });
-  // });
 });
 
+// Remove modal or book cards
 function close() {
-  let closeBtns = document.querySelectorAll('.close');
+  let closeBtns = document.querySelectorAll('.close-btn');
   closeBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-
       if (e.target.id === 'close-modal') {
-        let form = document.getElementById('modal-content');
+        let form = document.getElementById('modal-form');
         form.reset();
         modal.style.display = 'none';
       } else {
@@ -119,7 +103,6 @@ function close() {
         grandParent.removeChild(parent);
 
         for (let i = 0; i < myLibrary.length; i++) {
-          console.log(myLibrary[i].title, child.getAttribute('data-value'));
           if (myLibrary[i].title === child.getAttribute('data-value')) {
             delete myLibrary[i];
             myLibrary = myLibrary.filter((obj) => obj !== undefined);
@@ -130,10 +113,11 @@ function close() {
   });
 }
 
-close();
-
-let modal = document.getElementById('modal');
-let openModal = document.getElementById('open-modal');
+// Open modal to add new book
+let openModal = document.getElementById('add-book');
 openModal.addEventListener('click', () => {
+  close();
+  let modal = document.getElementById('modal');
   modal.style.display = 'block';
+
 });
